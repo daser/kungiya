8164cc3d9fd533671e2fedf1e6f2f5d9d81e4fe0
Daser David <daseronline@gmail.com>
2013-05-25 07:41:49 +0100

    after assets precompilation 2

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,8 @@ module Esusu
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += ['index.css']
+
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
